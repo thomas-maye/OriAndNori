8e731eddb708bacd6db139fbb11675c13a35596a
Mezanic <ezanic.mael@gmail.com>
2025-02-06 19:19:57 +0100

    Try to handle some issus after merge, for CSS

--- start/routes.ts
+++ start/routes.ts
@@ -8,19 +8,15 @@
 */
 
 import router from '@adonisjs/core/services/router'
-// import { middleware } from './kernel.js'
+import { middleware } from './kernel.js'
 
 const UsersController = () => import('#controllers/users_controller')
 const AuthController = () => import('#controllers/auth_controller')
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
 router.on('pets/create-pet').render('pages/create_pet')
 
 router.post('pets/create', [UsersController, 'createPet']).as('createPet')
-import { middleware } from '#start/kernel'
-
-router.on('/').render('pages/home').as('home')
-
 router.get('/register', [AuthController, 'register']).as('auth.register').use(middleware.guest())
 router.post('/register', [AuthController, 'handleRegister']).use(middleware.guest())
 router.get('/login', [AuthController, 'login']).as('auth.login').use(middleware.guest())
