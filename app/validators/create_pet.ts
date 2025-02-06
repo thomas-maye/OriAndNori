d21098c16a599b87a977cc31be4e8cd7466a6fe5
Mezanic <ezanic.mael@gmail.com>
2025-02-06 19:04:28 +0100

    Added pet_create validator and eslint fix for auth validator

--- app/validators/create_pet.ts
+++ app/validators/create_pet.ts
@@ -8,5 +8,7 @@ export const createPetValidator = vine.compile(
     vaccined: vine.boolean(),
     gender: vine.enum(['male', 'female']),
     description: vine.string().optional(),
+    breedName: vine.string(),
+    speciesName: vine.string(),
   })
 )
