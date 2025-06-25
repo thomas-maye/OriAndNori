cddfdb0e4b1ec88358332ac7867a05f635988477
Thomas Maye <thomas.maye.33@gmail.com>
2025-06-25 10:46:57 +0200

    Redirect to display my profile after successful profile update

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -186,6 +186,7 @@ export default class AuthController {
             .htmlView('emails/edit_myprofile', { user })
         })
         session.flash('success', 'Profile updated successfully')
+        return response.redirect().toRoute('auth.display_my_profile')
       } catch (error) {
         session.flash('error', 'Error sending confirmation email')
         return response.redirect().back()
